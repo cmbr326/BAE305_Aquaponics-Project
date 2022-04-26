8d3065714e83e410cfe7c6544f9297790253fcde
nbkrebs <102320028+nbkrebs@users.noreply.github.com>
2022-04-26 17:32:16 -0400

    Update ProjectCode.ino

--- ProjectCode.ino
+++ ProjectCode.ino
@@ -1,12 +1,9 @@
-int pump=7;
+int pump=7; //initialize pump, feeder, time, and block variables
 int feeder=6;
-int t=0;
+int t=0;    
 int block=0;
 
-int switchPinP = 2;
-int switchPinF = 1;
-
-#include <OneWire.h>
+#include <OneWire.h>  //initialize temperature sensor
 #include <DallasTemperature.h>
 #define ONE_WIRE_BUS 5
 OneWire oneWire(ONE_WIRE_BUS);
@@ -14,31 +11,26 @@ DallasTemperature sensors(&oneWire);
  float Celcius=0;
  float Fahrenheit=0;
  
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> //initialize LCD
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
-void setup() {
-pinMode(pump, OUTPUT);
- lcd.begin(16, 2); 
- lcd.clear();      
+void setup() {   
+pinMode(pump, OUTPUT);  //set motor pins as outputs
+pinMode(feeder, OUTPUT);
+ 
+lcd.begin(16, 2); //setup LCD
+lcd.clear();      
 lcd.setCursor(0, 0);                      
 lcd.print("Degrees F: ");                
 
-pinMode(feeder, OUTPUT);
-
-Serial.begin(9600);
+Serial.begin(9600); //start serial monitor
 sensors.begin();
-
-pinMode(switchPinP, INPUT_PULLUP);
-pinMode(switchPinF, INPUT_PULLUP);
 }
-void loop() {
-
-//lcd.clear();  
-lcd.setCursor(11,0);                            
+void loop() {  
+lcd.setCursor(11,0);    //print temperature to LCD                        
 lcd.print(Fahrenheit);      
 
-t=millis()/3600000;
+t=millis()/3600000; //start timer function for feeder
 Serial.println(t);
 Serial.println(block);
 if (t%12==0){
@@ -54,7 +46,7 @@ if (t%12==1){
   block=0;
 }
 
-sensors.requestTemperatures(); 
+sensors.requestTemperatures();  //read temperature and print to serial monitor
 Celcius=sensors.getTempCByIndex(0);
 Fahrenheit=sensors.toFahrenheit(Celcius);
 Serial.print(" C  ");
